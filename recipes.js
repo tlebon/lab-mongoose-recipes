69a686cc5c2ee00fa14e25d2644c98171509e999
tlebon <tlebon@gmail.com>
2018-08-23 14:32:47 +0200

    iteration two, stuck

--- recipes.js
+++ recipes.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js')
+const recipe = require('./Models/recipe.js')
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
     console.log('Connected to Mongo!')
-  }).catch(err => {
+    recipe.create({data})
+    .then((data)=>{console.log(data.title)})
+    // .catch((err) => {console.log("ERROR::",err)})
+  })
+
+  .catch(err => {
     console.error('Error connecting to mongo', err)
   });
-
